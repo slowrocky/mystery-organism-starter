12bde21fff4829a6820b18496a53bdab449ecf7a
slowrocky <matus.holotnak@gmail.com>
2021-07-05 00:05:57 +0200

    surviveFactory

--- main.js
+++ main.js
@@ -29,26 +29,71 @@ const pAequorFactory = (num, arr) => {
     compareDNA(pAequor) {
       let commonBase = 0;
       let matchDNA = 0;
-      for(let i=0; i < this.dna.length;i++){
-          if(this.dna[i] === pAequor.dna[i]){
-            commonBase++;
-          }
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === pAequor.dna[i]) {
+          commonBase++;
+        }
       }
-      matchDNA = Math.floor((100*commonBase)/this.dna.length);
+      matchDNA = Math.floor((100 * commonBase) / this.dna.length);
       console.log(`specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${matchDNA}% DNA in common`)
+    },
 
+    willLikelySurvive() {
+      let cBaseCounter = 0;
+      let gBaseCounter = 0;
+      let cBasePer = 0;
+      let gBasePer = 0;
+
+      for (let i = 0; i < this.dna.length; i++) {
+        switch (this.dna[i]) {
+          case 'C':
+            cBaseCounter++;
+            break;
+          case 'G':
+            gBaseCounter++;
+            break;
+          default:
+            break;
+        }
+      }
+      cbasePer = Math.floor(100 * cBaseCounter / this.dna.length);
+      gBasePer = Math.floor(100 * gBaseCounter / this.dna.length);
+
+      if (cbasePer >= 60 || gBasePer >= 60) {
+        return true;
+      }
+      return false;
     }
   }
 
   return obj.mutate();
 }
-let a = pAequorFactory(1, mockUpStrand());
-let b = pAequorFactory(2, mockUpStrand())
 
-console.log(a);
-console.log(b);
 
-a.compareDNA(b);
+// Generate array of set amount specimens, which can survive
+const surviveFactory = (number) => {
+  let surviveArray = [];
+  let counter = 0;
+
+  if (typeof number !== 'number') {
+    console.log('bad parameter!');
+    return;
+  }
+
+  while (counter < number) {
+    let specimen = pAequorFactory(counter, mockUpStrand());
+    if (specimen.willLikelySurvive()) {
+      surviveArray.push(specimen);
+      counter++;
+    }
+  }
+  return surviveArray;
+}
+
+
+// Print 30 survive specimen
+console.log(surviveFactory(30));
+
 
 
 
